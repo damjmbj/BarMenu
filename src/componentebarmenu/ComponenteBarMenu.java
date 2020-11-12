87c6c62838e540a14b71d59f47a4ceca8f3795c7
DAM-2 <DAM-2@PC19>
2020-11-12 13:50:51 +0100

    bar menu

---
+++ src/componentebarmenu/ComponenteBarMenu.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentebarmenu;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author DAM-2
+ */
+public class ComponenteBarMenu extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ComponenteBarMenu.class.getResource("view/ContextMenuOverview.fxml"));
+        
+        
+        BorderPane page = (BorderPane) loader.load();
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
