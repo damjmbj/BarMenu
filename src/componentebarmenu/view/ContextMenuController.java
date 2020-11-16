a5fab8ebfb20a3b9e4aaf65b909be29cda5418d6
DAM-2 <DAM-2@PC16>
2020-11-16 09:49:57 +0100

    Ejercicio9

--- src/componentebarmenu/view/ContextMenuController.java
+++ src/componentebarmenu/view/ContextMenuController.java
@@ -21,10 +21,11 @@ public class ContextMenuController implements Initializable {
     @FXML
     private Label label;
     
+    
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        label.setText("Hola!");
     }
     
     @Override
